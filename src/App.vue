97fcce484b51feed00ae07cc7c91d89d7be3ec8b
pieromarcoz <75951868+pieromarcoz@users.noreply.github.com>
2024-09-12 01:53:41 -0500

    first

---
+++ src/App.vue
@@ -0,0 +1,38 @@
+<script setup lang="ts">
+  import Header from './components/Header.vue'
+  import {ref, onMounted, computed} from 'vue'
+  import { db } from './data/db'
+  import CardProduct from './components/CardProduct.vue'
+
+  const guitarras = ref()
+
+  onMounted(() => {
+    guitarras.value = db
+  })
+
+
+</script>
+
+<template>
+  <Header/>
+  <main class="container-xl mt-5">
+    <h2 class="text-center">Nuestra Colección</h2>
+    <div class="row mt-5">
+      <CardProduct
+          v-for="guitarra in guitarras"
+          :guitarra="guitarra"
+      />
+    </div>
+  </main>
+
+
+  <footer class="bg-dark mt-5 py-5">
+    <div class="container-xl">
+      <p class="text-white text-center fs-4 mt-4 m-md-0">GuitarLA - Todos los derechos Reservados</p>
+    </div>
+  </footer>
+</template>
+
+<style scoped>
+
+</style>
---
+++ src/components/CardProduct.vue
@@ -0,0 +1,33 @@
+<script setup lang="ts">
+
+  interface Guitarra {
+    id: number,
+    nombre: string,
+    descripcion: string,
+    imagen: string,
+    precio: number
+  }
+
+  const props = defineProps<{
+    guitarra: Guitarra;
+  }>();
+
+  const {id, nombre, descripcion, imagen, precio} = props.guitarra
+
+</script>
+<template>
+  <div key="id" class="col-md-6 col-lg-4 my-4 row align-items-center">
+    <div class="col-4">
+      <img class="img-fluid" :src="`/img/${imagen}.jpg`" alt="imagen guitarra">
+    </div>
+    <div class="col-8">
+      <h3 class="text-black fs-4 fw-bold text-uppercase">{{ nombre }}</h3>
+      <p>{{ descripcion }}</p>
+      <p class="fw-black text-primary fs-3">{{ precio }}</p>
+      <button
+          type="button"
+          class="btn btn-dark w-100 "
+      >Agregar al Carrito</button>
+    </div>
+  </div>
+</template>
